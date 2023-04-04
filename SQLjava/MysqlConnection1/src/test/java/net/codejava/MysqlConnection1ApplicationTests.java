9b5685739ffca631cdc4ace63bf3617637d5503f
Hema B S <129481549+HemaBS1210@users.noreply.github.com>
2023-04-04 14:47:05 +0530

    Add files via upload

---
+++ SQLjava/MysqlConnection1/src/test/java/net/codejava/MysqlConnection1ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.codejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlConnection1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
