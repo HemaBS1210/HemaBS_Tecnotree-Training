13864e090bc1f8bed82f4955602d507c95508e68
Hema B S <129481549+HemaBS1210@users.noreply.github.com>
2023-04-04 16:23:28 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/ReverseString.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class ReverseString {
+	@GetMapping("/reverse")
+    public String reverseString(@RequestParam("input") String input) {
+        StringBuilder reversed = new StringBuilder(input).reverse();
+        return reversed.toString();
+    }
+}
+
+
+
+    
+
+
---
+++ demo/src/main/java/com/example/demo/WelcomeController.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+
+public class WelcomeController {
+   @GetMapping("/welcome")
+   public String welcome() {
+   return "welcome to spring boot app development";
+   }
+}
